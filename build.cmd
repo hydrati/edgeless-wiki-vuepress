cfdd68b517449d56d77796b98641c7014ff8264a
Cno <dsyourshy@qq.com>
2021-01-29 13:44:47 +0800

    修正推送脚本编码

--- build.cmd
+++ build.cmd
@@ -1,15 +1,15 @@
 @echo off
-title Buildingï¼ˆ1/4ï¼‰
+title Building£¨1/4£©
 cmd /c "yarn docs:build"
 
 cd /d "D:\Desktop\edgeless-wiki-vuepress\docs\.vuepress\dist"
 "C:\Program Files\7-Zip\7z.exe" a -tzip dist.zip *
-title Compressingï¼ˆ2/4ï¼‰
+title Compressing£¨2/4£©
 
 ssh root@148.70.88.72 "cd /www/admin/wiki.edgeless.top_80/wwwroot/v2;rm -rf *"
-title Uploadingï¼ˆ3/4ï¼‰
+title Uploading£¨3/4£©
 scp "D:\Desktop\edgeless-wiki-vuepress\docs\.vuepress\dist\dist.zip" root@148.70.88.72:/www/admin/wiki.edgeless.top_80/wwwroot/v2
-title Unzipingï¼ˆ4/4ï¼‰
+title Unziping£¨4/4£©
 ssh root@148.70.88.72 "cd /www/admin/wiki.edgeless.top_80/wwwroot/v2;unzip ./dist.zip"
 title Finish
 timeout 5
