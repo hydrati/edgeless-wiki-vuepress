c291a46b49881c5fa6bd5cc15e2d720466d90f18
Cno <dsyourshy@qq.com>
2021-01-30 20:50:03 +0800

    增加源代码的update

--- build.cmd
+++ build.cmd
@@ -1,15 +1,20 @@
 @echo off
-title Building£¨1/4£©
+title Updating(1/5)
+git add *.md
+git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%"
+git push
+
+title Building(2/5)
 cmd /c "yarn docs:build"
 
 cd /d "D:\Desktop\edgeless-wiki-vuepress\docs\.vuepress\dist"
 "C:\Program Files\7-Zip\7z.exe" a -tzip dist.zip *
-title Compressing£¨2/4£©
+title Compressing(3/5)
 
 ssh root@148.70.88.72 "cd /www/admin/wiki.edgeless.top_80/wwwroot/v2;rm -rf *"
-title Uploading£¨3/4£©
+title Uploading(4/5)
 scp "D:\Desktop\edgeless-wiki-vuepress\docs\.vuepress\dist\dist.zip" root@148.70.88.72:/www/admin/wiki.edgeless.top_80/wwwroot/v2
-title Unziping£¨4/4£©
+title Unziping(5/5)
 ssh root@148.70.88.72 "cd /www/admin/wiki.edgeless.top_80/wwwroot/v2;unzip ./dist.zip"
 title Finish
 timeout 5
