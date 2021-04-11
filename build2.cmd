8bc53b1d9dfcb3aeb34058874ebfa8581981065c
Cno <dsyourshy@qq.com>
2021-04-11 11:45:59 +0800

    Test for CI

---
+++ build2.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+title Updating
+git add *.md
+git add *.cmd
+git add *.js
+git add img
+git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%"
+git push
