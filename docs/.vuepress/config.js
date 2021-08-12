d22ae816e402ea9fdac790453040b8ecf8bfbf9f
L0serj3rry <dsyourshy@qq.com>
2021-08-12 13:38:31 +0800

    20210812_13383126

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -90,7 +90,7 @@ module.exports = {
               { title: "常规开发", path: "/develop/plugin" },
               { title: "自动构建", path: "/develop/automake" },
               { title: "暴力封装", path: "/develop/force" },
-              { title: ".net插件包", path: "/develop/net" },
+              // { title: ".net插件包", path: "/develop/net" },
             ],
           },
           {
