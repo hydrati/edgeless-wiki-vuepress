d0459f9de90e7e140b4b1c81e64a0884263aeecc
Cno <dsyourshy@qq.com>
2021-08-24 01:29:02 +0800

    20210824_ 1290251

--- build2.cmd
+++ build2.cmd
@@ -4,6 +4,6 @@ title Updating
 git add *.md
 git add *.cmd
 git add *.js
-git add img
+git add picbed
 git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,2%"
 git push
